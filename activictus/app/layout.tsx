4df1a24f706fcd815329512c0f5558431806849e
daanmooij <daanmooij@icloud.com>
2025-02-01 13:04:11 +0100

    Deleted some more unnecessary files

--- activictus/app/layout.tsx
+++ activictus/app/layout.tsx
@@ -1,11 +1,3 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { Geist } from "next/font/google";
-import { ThemeProvider } from "next-themes";
-import Link from "next/link";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -18,31 +10,19 @@ export const metadata = {
   description: "Onze plek om aan te geven wanneer we waar zijn!",
 };
 
-const geistSans = Geist({
-  display: "swap",
-  subsets: ["latin"],
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <div className="gap-20 max-w-5xl p-5">{children}</div>
-            </div>
-          </main>
-        </ThemeProvider>
+    <html lang="en">
+      <body>
+        <main className="min-h-screen">
+          <div className="flex-1 w-full flex flex-col gap-20 items-center">
+            <div className="gap-20 max-w-5xl p-5">{children}</div>
+          </div>
+        </main>
       </body>
     </html>
   );
